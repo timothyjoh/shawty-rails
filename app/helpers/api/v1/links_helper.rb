c9bf8c1795babc5606ff15046a36d27453f84122
Tim Johnson <jhnsntmthy@gmail.com>
2021-11-21 20:22:05 -0500

    remove unused helpers

--- app/helpers/api/v1/links_helper.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Api
-  module V1
-    # Links Helpers
-    module LinksHelper
-    end
-  end
-end
--- app/helpers/application_helper.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-# Base Helper class
-module ApplicationHelper
-end
